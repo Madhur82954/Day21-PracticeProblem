97d3d1b9a4510823113dd7e4e38c3bf279b42447
Madhur82954 <itzmadhurverma@gmail.com>
2023-10-19 12:52:57 +0530

    [Madhur] - Day21-PracticeProblem Added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day21_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer.Insert();
+            Customer.Retrieve();
+            Customer.Delete();
+            Customer.Update();
+        }
+    }
+}
